3effbe549d96ab39390c79c86e0d492e85b4dcf0
Yvan <YvanKostenevich@gmail.com>
2019-10-12 22:23:51 +0300

    feat: add solution to task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,36 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
+function check(str, bracketsConfig) {
+
+    let openingBrackets = bracketsConfig.map(function (element) {
+        return element[0];
+    });
+
+    let closingBrackets = bracketsConfig.map(function (element) {
+        return element[1];
+    });
+
+    let bracketStack = [];
+
+    for (let i = 0; i < str.length; i++) {
+
+        if (openingBrackets.includes(str[i])) {
+            if (closingBrackets.includes(str[i]) && bracketStack[bracketStack.length - 1] === str[i]) {
+                bracketStack.pop();
+                continue;
+            }
+            bracketStack.push(str[i]);
+
+        } else if (closingBrackets.includes(str[i])) {
+
+            if (openingBrackets.indexOf(bracketStack[bracketStack.length - 1]) === closingBrackets.indexOf(str[i])) {
+                bracketStack.pop();
+            } else {
+                return false;
+            }
+        }
+    }
+
+    return bracketStack.length === 0;
+
 }
+
+module.exports = check;
